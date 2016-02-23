12f6c9d144605f0c1dc0d9f2ecba9fc8f7b515fb
naminh <minh.nguyenanh@axonactive.vn>
2016-02-23 14:48:11 +0700

    add new proj

---
+++ WatchKitUtils/WatchKitUtils/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WatchKitUtils
+//
+//  Created by Nguyen Anh Minh on 2/23/16.
+//  Copyright © 2016 Minh Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
